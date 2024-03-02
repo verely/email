dd066ab56289eaa0c2421553b43f01994ee6cf6c
Valery <verelly.v@gmail.com>
2024-03-02 16:25:44 +0200

    update EmailPreview component to display email details including a formatted date

--- src/cmps/EmailPreview.jsx
+++ src/cmps/EmailPreview.jsx
@@ -1,6 +1,12 @@
-export function EmailPreview({email}){
-    return <article className="email-preview">
-        <h2>{email.subject}</h2>
-        <h4>{email.from}</h4>
+export function EmailPreview({ email }) {
+
+  const sent  = new Date(email.sentA).toLocaleString();
+  return (
+    <article className="email-preview">
+      <span className="star">★</span>
+      <h4 className="from">{email.from}</h4>
+      <h4 className="subject">{email.subject}</h4>
+      <h4 className="sent">{sent}</h4>
     </article>
+  );
 }
